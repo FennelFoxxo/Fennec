6ff7130ad66abb030e024738f5ee5dbd52a9a6f5
Fennelfoxxo <fennelfoxxo@gmail.com>
2025-04-22 16:27:00 -0700

    demonstrate passing args to function via regs

--- system/projects/child_thread/src/main.c
+++ system/projects/child_thread/src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+int initialized = 0;
+
 int main(int argc, char *argv[]) {
+    // Name is first argument
+    char* name = argv[0];
+
+    printf("Hello from %s!\n", name);
+
+    initialized = 1;
+
+    while (1);
+    return 0;
+}
+
+int main2(int argc, char *argv[]) {
+    // Wait for the first thread to initialize musllibc
+    while (!initialized);
+
     // Name is first argument
     char* name = argv[0];
 
