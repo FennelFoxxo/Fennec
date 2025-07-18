f74e46027148f51b479f87fe2a2a7b2f55021b9b
Fennelfoxxo <fennelfoxxo@gmail.com>
2025-07-17 21:12:29 -0700

    update to add gp memory passing

--- system/projects/child_thread/src/main.c
+++ system/projects/child_thread/src/main.c
@@ -2,19 +2,28 @@
 #include <sel4/sel4.h>
 #include <sel4runtime.h>
 #include <string.h>
+#include <stdlib.h>
 
 int initialized = 0;
 
 #define MIN_ALIGN_BYTES 16
 #define MIN_ALIGNED __attribute__((aligned (MIN_ALIGN_BYTES)))
-static char tls0[4096] MIN_ALIGNED = {};
-static char tls1[4096] MIN_ALIGNED = {};
 
 void produce(const char* s);
 void consume();
 
-int main(int argc, char *argv[]) {
+int startswith(const char *str, const char *prefix) {
+    return strncmp(prefix, str, strlen(prefix)) == 0;
+}
+
+
+int main(int argc, char *argv[], char *envp[]) {
     printf("Started thread: %s!\n", argv[0]);
+    printf("  Envp: ");
+    for (char** s = envp; *s; s++) {
+        printf("%s ", *s);
+    }
+    printf("\n");
     initialized = 1;
 
     while (1) {
@@ -24,25 +33,50 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-int main2(int argc, char *argv[]) {
+int main2(int argc, char *argv[], char *envp[]) {
     // Wait for the first thread to initialize musllibc
     while (!initialized);
-    
+
     printf("Started thread: %s\n", argv[0]);
+
     printf("  Args: ");
     for (int i = 1; i < argc; i++) {
         printf("%s ", argv[i]);
     }
     printf("\n");
 
-    // Normally sel4runtime would set up the tls, but since main2 is called directly,
-    // we need to do it manually
-    if (strcmp(argv[0], "producer1_thread") == 0) {
-        sel4runtime_move_initial_tls(tls0);
-        __sel4_ipc_buffer = (void*)0x528000;
-    } else {
-        sel4runtime_move_initial_tls(tls1);
-        __sel4_ipc_buffer = (void*)0x52b000;
+    char tls[4096] MIN_ALIGNED = {};
+    sel4runtime_move_initial_tls(tls);
+
+    // Environment pointers
+    for (char** env_ptr = envp; *env_ptr; env_ptr++) {
+        char* env_var = *env_ptr;
+        // Look for ipc buffer environment pointer
+        if (startswith(env_var, "ipc_buffer=")) {
+            // Convert to int
+            char* ipc_buffer_addr_str = strchr(env_var, '=') + 1;
+            long long unsigned ipc_buffer_addr = atoll(ipc_buffer_addr_str);
+
+            // Set ipc buffer
+            __sel4_ipc_buffer = (void*)ipc_buffer_addr;
+
+        }
+
+        if (startswith(env_var, "gp_memory_info=")) {
+            // Convert to int
+            char* gp_memory_info_addr_str = strchr(env_var, '=') + 1;
+            long long unsigned gp_memory_info_addr = atoll(gp_memory_info_addr_str);
+
+            printf("Received gp memory info at addr %llx\n", gp_memory_info_addr);
+
+            seL4_Word num_untypeds = *(seL4_Word*)gp_memory_info_addr;
+            printf("Num untypeds: %lu\n", num_untypeds);
+            for (int i = 0; i < num_untypeds; i++) {
+                seL4_Word size_bits = ((seL4_Word*)gp_memory_info_addr)[i+1];
+                printf("Untyped %d size bits: %lu\n", i, size_bits);
+
+            }
+        }
     }
 
     char buffer[100];
